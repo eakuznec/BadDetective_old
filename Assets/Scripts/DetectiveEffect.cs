80ea85954b464590f50d54664d378b07732482b1
eakuznec <eakuznec@gmail.com>
2018-09-30 23:49:02 +0300

    DetectiveEffect

---
+++ Assets/Scripts/DetectiveEffect.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BadDetective
+{
+    public class DetectiveEffect : MonoBehaviour
+    {
+        DetectiveEffectType type;
+        Trait trait;
+        int value;
+        float floatValue;
+        bool flag;
+        
+        public void Realize(Detective detective)
+        {
+            if(type == DetectiveEffectType.CHANGE_HEALTH)
+            {
+                detective.ChangeCurHealth(value);
+            }
+            else if(type == DetectiveEffectType.CHANGE_STRESS)
+            {
+                detective.ChangeCurStress(value);
+            }
+            else if(type == DetectiveEffectType.CHANGE_LOYALTY)
+            {
+                detective.ChangeCurLoyalty(value);
+            }
+            else if(type == DetectiveEffectType.CHANGE_CONFIDENCE)
+            {
+                detective.ChangeCurConfidence(value);
+            }
+            else if(type == DetectiveEffectType.CHANCE_ADD_TRAIT)
+            {
+                float chance = Random.Range(0, 100f);
+                if (chance <= floatValue)
+                {
+                    detective.AddTrait(trait);
+                }
+            }
+        }
+    }
+
+    public enum DetectiveEffectType
+    {
+        NONE,
+        CHANGE_HEALTH,
+        CHANGE_STRESS,
+        CHANGE_CONFIDENCE,
+        CHANGE_LOYALTY,
+        CHANCE_ADD_TRAIT,
+        
+    }
+}
---
+++ Assets/Scripts/EquipmentContainer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BadDetective
+{
+    public class EquipmentContainer : MonoBehaviour
+    {
+        public Equipment equipment;
+        public Detective owner;
+
+        private void Start()
+        {
+            AttachEffects();
+        }
+
+        public Trait trait
+        {
+            get
+            {
+                if (equipment != null)
+                {
+                    return equipment.trait;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void AttachEffects()
+        {
+            if (HaveTag(Tag.NULL))
+            {
+                equipment.trait.AttachEffects(owner);
+            }
+        }
+
+        public void DetachEffects()
+        {
+            if (HaveTag(Tag.NULL))
+            {
+                equipment.trait.DetachEffects(owner);
+            }
+        }
+
+        public List<Tag> GetTags()
+        {
+            return equipment.trait.tags;
+        }
+
+        public bool HaveTag(Tag tag)
+        {
+            if (tag == Tag.NULL)
+            {
+                return GetTags().Count == 0;
+            }
+            else
+            {
+                return GetTags().Contains(tag);
+            }
+        }
+    }
+}
